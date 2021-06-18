a8aaf97ac7eb512644f76dcda90703b2d2b16936
Darklord20105 <darklord20105@gmail.com>
2021-06-18 16:30:05 +0300

    code modify

--- server.js
+++ server.js
@@ -2,11 +2,31 @@
 require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
+var expect      = require('chai').expect;
 const cors        = require('cors');
+var mongoose    = require('mongoose')
+var helmet      = require('helmet')
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+
+mongoose.Promise = global.Promise
+const dbName = 'stock-price-database'
+mongoose.connect(`${process.env.DB}${dbName}`, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', err => { console.error(err) })
+db.once('open', () => {
+  console.log('Connected to ' + dbName)
+})
+
+// Close MongoDB connection
+process.on('SIGINT', () => {
+  db.close(() => {
+    console.log(`Closing connection to ${dbName}`)
+    process.exit(0)
+  })
+})
 
 const app = express();
 
@@ -16,6 +36,8 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet())
 
 //Index page (static HTML)
 app.route('/')
